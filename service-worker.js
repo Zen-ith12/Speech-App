449a1da22c89428b2c25d1e35a15990a55b052bc
Zen-ith12 <joelsamano282@gmail.com>
2025-07-02 22:57:09 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,3 +12,22 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('jw-speech-cache').then(function(cache) {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './jw_speech_icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+}); 
